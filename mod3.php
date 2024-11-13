55b41c6a7cda4349951bce636d8a965796dd8b49
alfariz7704 <farizrasyid084@gmail.com>
2024-11-13 09:48:50 +0700

    Add files via upload

---
+++ mod3.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Beranda</title>
+<link rel="stylesheet" href="modul3.css">
+</head>
+<body>
+
+<div id="wrapper">
+	<div id="container">
+		<header></header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Beranda</a></li>
+                <li><a href="#">Artikel</a></li>
+                <li><a href="#">Profil</a>
+                    <ul class="submenu">
+                        <li><a href="#">Hubungi Kami</a></li>
+                        <li><a href="#">Tentang Kami</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <article>
+            <div id="single">
+				<div id="t-images">
+					<img src="../image/gbrweb.jpg" alt="Gambar Web">
+				</div>
+                <h2>Belajar Web dengan HTML+CSS3</h2>
+                <p>Belajar web dengan HTML dan CSS3 adalah langkah awal yang penting dalam pengembangan web. HTML (HyperText Markup Language) digunakan untuk menyusun struktur dasar halaman, seperti judul, paragraf, dan gambar. CSS3 (Cascading Style Sheets) digunakan untuk menambahkan gaya dan tata letak, seperti warna, ukuran font, dan tata letak responsif. Keduanya saling melengkapi agar halaman web terlihat menarik dan profesional....</p>
+                <div id="more"><a href="article1.php">Baca selengkapnya</a></div>
+            </div>
+				<div id="single">
+					<div id="t-images">
+				<img src="../image/mockup.jpg" alt="Gambar Web">
+				</div>
+                <h2>Mockup web dengan Word</h2>
+                <p>Membuat mockup web dengan Word adalah langkah awal yang sederhana untuk merancang tampilan situs web. Dengan Word, Anda bisa menyusun elemen-elemen dasar seperti header, navigasi, gambar, dan konten utama secara visual. Cukup gunakan fitur shapes dan text boxes untuk mengatur letak setiap elemen, sehingga membantu tim atau klien memvisualisasikan desain awal tanpa perlu langsung menulis kode....</p>
+                <div id="more"><a href="article1.php">Baca selengkapnya</a></div>
+			</div>
+        </article>
+        <aside>
+            <li class="widget">
+                <h2>Berita Populer</h2>
+                <ul>
+                    <li><a href="#">Belajar Web dengan HTML+CSS3</a></li>
+                    <li><a href="#">Mockup web dengan Word</a></li>
+                </ul>
+            </li>
+            <li class="widget">
+                <h2>Artikel Populer</h2>
+                <ul>
+                    <li><a href="#">Artikel 1</a></li>
+                    <li><a href="#">Artikel 2</a></li>
+                    <li><a href="#">Artikel 3</a></li>
+                </ul>
+            </li>
+            <li class="widget">
+                <h2>Berita Selanjutnya</h2>
+                <ul>
+                    <li><a href="#">UINSU Ganti Logo</a></li>
+                    <li><a href="#">RUSIA Damai</a></li>
+                    <li><a href="#">Cuaca di Medan</a></li>
+                </ul>
+            </li>
+			
+        </aside>
+        <footer>
+            <div id="column">
+                <h2>LAYANAN</h2>
+                E-Mail<br>
+                E-Learning<br>
+                E-Repository<br>
+                E-Jurnal<br>
+                Digital Library<br>
+                Portal SIA<br>
+                SI-Selma<br>
+                SI-Dahlia
+            </div>
+
+            <div id="column">
+                <h2>Business Hour</h2>
+                Silahkan hubungi pusbangnis UINSU telp 064123123 pada jam kerja atau email <a href="#">pusbangnis@uinsu.ac.id</a>
+            </div>
+
+            <div id="column">
+                <h2>Head Office</h2>
+                Jl. Willem Iskandar, Pasar V, Medan Estate
+                Tel:+62-61-6615683
+                Email: humas@uinsu.ac.id
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
